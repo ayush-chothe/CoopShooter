3850678f071ecf7dea68e9ba0ccb60f650070c82
ayush-chothe <53037930+ayush-chothe@users.noreply.github.com>
2020-06-04 14:52:50 +0530

    Initial commit

---
+++ Source/CoopShooter/CoopShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPSHOOTER_API ACoopShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
